82d0be5eec6a02887af79d036c3f8c8fe46ef47a
Neil Flood <Neil.Flood@derm.qld.gov.au>
2011-12-13 09:58:47 +1000

    Added a simple (hopefully) test harness, and a single test, of averaging two rasters

---
+++ rios/bin/testrios.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Main test harness for RIOS. 
+
+Should be run as a main program. It then runs a selection 
+of tests of some capabilities of RIOS. 
+
+"""
+
+import testavg
+testavg.run()
+
