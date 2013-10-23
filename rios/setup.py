68f547d2ed4a3f3a6f4df6b280c0fd2e3d1420ae
Neil Flood <24580585+neilflood@users.noreply.github.com>
2013-10-23 11:35:45 +1000

    Changed version number to 1.1.5

--- rios/setup.py
+++ rios/setup.py
@@ -9,7 +9,7 @@ import glob
 from distutils.core import setup
 
 setup(name='rios',
-      version='1.1.4',
+      version='1.1.5',
       description='Raster Input/Output Simplification',
       author='Sam Gillingham',
       author_email='gillingham.sam@gmail.com',
