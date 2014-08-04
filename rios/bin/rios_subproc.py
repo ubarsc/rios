2849cde1b24083ee97093823d85791e8e22ffcf4
Neil Flood <24580585+neilflood@users.noreply.github.com>
2014-08-04 15:48:35 +1000

    Delete the input pickle file as soon as it is read. Also added better comments.

--- rios/bin/rios_subproc.py
+++ rios/bin/rios_subproc.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 from rios import applier
 
 import sys
+import os
 try:
     import cPickle as pickle
 except ImportError:
@@ -25,7 +26,15 @@ def mainRoutine():
     if nArgs == 2:
         outf = open(sys.argv[2], 'w')
 
+    # Read the pickled input
     (fn, a) = pickle.load(inf)
+    
+    # If using a disk file for input, close it and remove it now that we have read it
+    if nArgs >= 1:
+        inf.close()
+        os.remove(sys.argv[1])
+    
+    # Decompose the input data and assemble arguments to pass to user function
     info = a[0]
     inputs = a[1]
     outputs = applier.BlockAssociations()
@@ -33,8 +42,10 @@ def mainRoutine():
     if len(a) > 2:
         funcArgs += (a[2],)
     
+    # Execute the function, with the given input data
     fn(*funcArgs)
     
+    # Pickle and write out the output
     pickle.dump(outputs, outf)
 
 if __name__ == "__main__":
