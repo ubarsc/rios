b1ba00b147edd5e2286f0ca522c48deba31682cf
Neil Flood <Neil.Flood@derm.qld.gov.au>
2015-03-16 07:48:22 +1000

    Removed stoopid test line (sigh....)

--- rios/rios/parallel/cloud_playdoh/cloudpickle.py
+++ rios/rios/parallel/cloud_playdoh/cloudpickle.py
@@ -27,7 +27,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this package; if not, see
 http://www.gnu.org/licenses/lgpl-2.1.html
 """
-somecrap
+
 import ctypes
 import os
 import pickle
