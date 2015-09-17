3b558763a441d189dd24c7db9c4aa1c1ab2db13f
Sam Gillingham <10858582+gillins@users.noreply.github.com>
2015-09-17 16:15:41 +1000

    re-worked parallel implementation to use objects derived from the JobInfo class which hides the actual number of parameters and details of them from the jobmanager. This in theory makes the jobmanager useful outside of RIOS.

--- rios/bin/rios_subproc.py
+++ rios/bin/rios_subproc.py
@@ -3,52 +3,38 @@
 Main program for RIOS subprocesses. 
 
 """
+# This file is part of RIOS - Raster I/O Simplification
+# Copyright (C) 2012  Sam Gillingham, Neil Flood
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 
-from rios import applier
+from rios.parallel import subproc
 
 import sys
-import os
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
-def mainRoutine():
-    """
-    Main program
-    """
+if __name__ == "__main__":
     nArgs = len(sys.argv) - 1
     # use binary buffer objects
     # otherwise unpickling fails
     inf = sys.stdin.buffer
     outf = sys.stdout.buffer
+    inFileName = None
     if nArgs >= 1:
-        inf = open(sys.argv[1], 'rb')
+        inFileName = sys.argv[1]
+        inf = open(inFileName, 'rb')
     if nArgs == 2:
         outf = open(sys.argv[2], 'wb')
 
-    # Read the pickled input
-    (fn, a) = pickle.load(inf)
-    
-    # If using a disk file for input, close it and remove it now that we have read it
-    if nArgs >= 1:
-        inf.close()
-        os.remove(sys.argv[1])
-    
-    info = a[0]
-    inputs = a[1]
-    # TODO: create blank output in general way
-    outputs = applier.BlockAssociations()
-    funcArgs = (info, inputs, outputs)
-    if len(a) > 2:
-        funcArgs += (a[2],)
-    
-    # Execute the function, with the given input data
-    fn(*funcArgs)
-    
-    # Pickle and write out the output
-    pickle.dump(outputs, outf)
-
-if __name__ == "__main__":
-    mainRoutine()
+    subproc.runJob(inf, outf, inFileName)
