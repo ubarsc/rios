bbbca26bf01c1fc566f01df97c4f990d1e92a13a
Neil Flood <24580585+neilflood@users.noreply.github.com>
2019-03-21 08:20:56 +1000

    vectorreader.py: Removed unused import

--- rios/vectorreader.py
+++ rios/vectorreader.py
@@ -22,7 +22,6 @@ ImageReader and ImageWriter classes.
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
-import sys
 import tempfile
 import subprocess
 from .imagewriter import DEFAULTDRIVERNAME
