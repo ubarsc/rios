a9dfb12396f2a350f7c0313942a0f9b701ac91fa
Sam Gillingham <gillingham.sam@gmail.com>
2023-08-01 11:31:33 +1000

    got stack working

--- rios/parallel/aws/templates/createbatch.py
+++ rios/parallel/aws/templates/createbatch.py
@@ -10,12 +10,19 @@ import argparse
 import boto3
 
 N_AZS = 3
+DFLT_STACK_NAME = 'RIOS'
+DFLT_REGION = 'ap-southeast-2'
+
 
 def getCmdArgs():
     """
     Get Command Line Args
     """
     p = argparse.ArgumentParser()
+    p.add_argument('--stackname', default=DFLT_STACK_NAME,
+        help="Name of CloudFormation Stack to create. (default=%(default)s)")
+    p.add_argument('--region', default=DFLT_REGION,
+        help="AWS Region to use. (default=%(default)s)")
     p.add_argument('--az', action='append',
         help="Availability zones to use. Specify {} times".format(N_AZS))
     p.add_argument('--ecrname',
@@ -31,52 +38,65 @@ def getCmdArgs():
         help="Wait until CloudFormation is complete before exiting")
         
     cmdargs = p.parse_args()
-    if cmdargs.az is not None and len(az) != 3:
-        raise SystemExit("--az must be specified {3} times".format(N_AZS))
+    if cmdargs.az is not None and len(cmdargs.az) != 3:
+        raise SystemExit("--az must be specified {} times".format(N_AZS))
         
     return cmdargs
     
     
 def main():
+    """
+    Main function for this script
+    """
     cmdargs = getCmdArgs()
     
-    stackId, status = createBatch(cmdargs.az, cmdargs.ecrname, cmdargs.vcpus, 
-        cmdargs.mem, cmdargs.maxjobs, cmdargs.wait):
+    stackId, status = createBatch(cmdargs.stackname, cmdargs.region,
+        cmdargs.az, cmdargs.ecrname, cmdargs.vcpus, 
+        cmdargs.mem, cmdargs.maxjobs, cmdargs.wait)
             
-    print('stackId: {}'.format(stackId)})
+    print('stackId: {}'.format(stackId))
     if status is not None:
         print('status: {}'.format(status))
 
 
 def addParam(params, key, value):
+    """
+    Helper function for adding keys/values in the format
+    boto3 expects.
+    """
     params.append({'ParameterKey': key,
         'ParameterValue': value})
 
     
-def createBatch(azs, ecrName, vCPUs, maxMem, maxJobs, wait):
+def createBatch(stackname, region, azs, ecrName, vCPUs, maxMem, maxJobs, wait):
+    """
+    Do the work of creating the CloudFormation Stack
+    """        
     
     params = []
     if azs is not None:
-        addParam(params, 'AZ_1', azs[0])
-        addParam(params, 'AZ_2', azs[1])
-        addParam(params, 'AZ_3', azs[2])
+        addParam(params, 'AZ1', azs[0])
+        addParam(params, 'AZ2', azs[1])
+        addParam(params, 'AZ3', azs[2])
         
-    if cmdargs.ecrname is not None:
+    if ecrName is not None:
         addParam(params, 'ECR_Name', ecrName)
         
-    if cmdargs.vcpus is not None:
+    if vCPUs is not None:
         addParam(params, 'VCPUS', vCPUs)
         
-    if cmdargs.mem is not None:
+    if maxMem is not None:
         addParam(params, 'MaxMemory', maxMem)
         
-    if cmdargs.maxjobs is not None:
+    if maxJobs is not None:
         addParam(params, 'MaxJobs', maxJobs)
-    
-    client = boto3.client('cloutformation')
-    resp = client.create_stack(StackName='RIOS',
-        TemplateURL='file://batch.yaml',
-        Parameters=[params])
+        
+    body = open('batch.yaml').read()
+        
+    client = boto3.client('cloudformation', region_name=region)
+    resp = client.create_stack(StackName=stackname,
+        TemplateBody=body, Capabilities=['CAPABILITY_IAM'], 
+        Parameters=params)
         
     stackId = resp['StackId']
     print('StackId: {}'.format(stackId))
@@ -84,15 +104,15 @@ def createBatch(azs, ecrName, vCPUs, maxMem, maxJobs, wait):
     status = None
     if wait:
         while True:
-            resp = client.descripe_stacks(StackName=stackId)
+            time.sleep(30)
+            resp = client.describe_stacks(StackName=stackId)
             status = resp['Stacks'][0]['StackStatus']
             if status != 'CREATE_IN_PROGRESS':
                 break
-            time.sleep(30)
+            print('Status: {}'.format(status))
                 
     return stackId, status
     
 
 if __name__ == '__main__':
     main()
-    
---
+++ rios/parallel/aws/templates/deletebatch.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+Helper script to delete the RIOS Batch CloudFormation. 
+Optionally wait until stack is created before returning.
+"""
+
+import time
+import argparse
+import boto3
+import botocore
+
+DFLT_STACK_NAME = 'RIOS'
+DFLT_REGION = 'ap-southeast-2'
+
+
+def getCmdArgs():
+    """
+    Get Command Line Args
+    """
+    p = argparse.ArgumentParser()
+    p.add_argument('--stackname', default=DFLT_STACK_NAME,
+        help="Name of CloudFormation Stack to delete. (default=%(default)s)")
+    p.add_argument('--region', default=DFLT_REGION,
+        help="AWS Region to use. (default=%(default)s)")
+    p.add_argument('--wait', action="store_true",
+        help="Wait until CloudFormation is complete before exiting")
+        
+    cmdargs = p.parse_args()
+        
+    return cmdargs
+
+
+def main():
+    """
+    Main function for this script
+    """
+    cmdargs = getCmdArgs()
+
+    client = boto3.client('cloudformation', region_name=cmdargs.region)
+    client.delete_stack(StackName=cmdargs.stackname)
+    
+    if cmdargs.wait:
+        while True:
+            time.sleep(30)
+            try:
+                client.describe_stacks(StackName=cmdargs.stackname)
+            except botocore.exceptions.ClientError:
+                break
+
+
+if __name__ == '__main__':
+    main()
