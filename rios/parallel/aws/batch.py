fb648e8348e4b6ba6b528cd75d572ffaed765f71
Sam Gillingham <gillingham.sam@gmail.com>
2023-09-25 15:46:11 +1000

    Various improvements to AWS Batch support (#67)
    * add support for setting region and having a 'main' repo
    
    * tidy
    
    * add region suport
    
    * fix permissions and regions
    
    * RIOS_BATCH_REGION -> RIOS_AWSBATCH_REGION
    
    * RIOS_BATCH_STACK -> RIOS_AWSBATCH_STACK

--- rios/parallel/aws/batch.py
+++ rios/parallel/aws/batch.py
@@ -23,11 +23,12 @@ to create a separate VPC with all the infrastructure required. It is recommended
 to use the script `templates/createbatch.py` for the creation or modification (via the ``--modify``
 command line option) of this CloudFormation stack. There are also options for
 overriding some of the input parameters - see the output of `createbatch.py --help`
-for more information.
+for more information. NB: when running in a region that is NOT ap-southeast-2 you will
+need to update the availability zones (--az command line option).
 
 When you have completed processing you can run ``templates/deletebatch.py`` to delete
 all resources so you aren't paying for it. Note that you specify the region and stack
-name for this script via the RIOS_BATCH_REGION and RIOS_BATCH_STACK environment variables.
+name for this script via the RIOS_AWSBATCH_REGION and RIOS_AWSBATCH_STACK environment variables.
 
 Note that both ``createbatch.py`` and ``deletebatch.py`` have a ``--wait`` option that causes the
 script to keep running until creation/deletion is complete. 
@@ -39,25 +40,26 @@ AWS Batch requires you to provide a Docker image with the required software inst
 A `Dockerfile` is provided for this, but it it recommended that you use the `Makefile`
 to build the image as this handles the details of pulling the names out of the CloudFormation
 stack and creating a tar file of RIOS for copying into the Docker image. To build and push to 
-ECR simply run::
+ECR simply run (being careful to set RIOS_AWSBATCH_REGION to the correct AWS region)::
 
-    make
+    RIOS_AWSBATCH_REGION=ap-southeast-2 make
 
 By default this image includes GDAL, boto3 and RIOS. 
 
 Normally your script will need extra packages to run. You can specify the names of Ubuntu packages
 to also install with the environment variable `EXTRA_PACKAGES` like this::
 
-    EXTRA_PACKAGES="python3-sklearn python3-skimage" make
+    EXTRA_PACKAGES="python3-sklearn python3-skimage" RIOS_AWSBATCH_REGION=ap-southeast-2 make
 
 
 You can also use the `PIP_PACKAGES` environment variable to set the name of any pip packages like this::
 
-    PIP_PACKAGES="pydantic python-dateutil" make
+    PIP_PACKAGES="pydantic python-dateutil" RIOS_AWSBATCH_REGION=ap-southeast-2 make
 
 You can also specify both if needed::
 
-    EXTRA_PACKAGES="python3-sklearn python3-skimage" PIP_PACKAGES="pydantic python-dateutil" make
+    EXTRA_PACKAGES="python3-sklearn python3-skimage" PIP_PACKAGES="pydantic python-dateutil" \
+        RIOS_AWSBATCH_REGION=ap-southeast-2 make
 
 Setting up your main script
 ---------------------------
@@ -93,11 +95,11 @@ Don't forget to pass in your ``AWS_ACCESS_KEY_ID`` and ``AWS_SECRET_ACCESS_KEY``
 container when it runs (these variables are automatically set if running as a AWS Batch job but you'll
 need to set them otherwise).
 
-Also a good idea to pass in your RIOS_BATCH_REGION and RIOS_BATCH_STACK environment variables if the
+Also a good idea to pass in your RIOS_AWSBATCH_REGION and RIOS_AWSBATCH_STACK environment variables if the
 defaults have been overridden so that RIOS can find the CloudFormation stack.
 
-Needless to say the account that this "main" script run as should have sufficient permissions on the resources 
-created by CloudFormation. 
+To also run you "main" Dockerfile as a batch job, push to the "RIOSecrMain" repository created by 
+``templates/batch.yaml``. You can then submit jobs to the RIOSJobQueue using the RIOSJobDefinitionMain.
  
 """
 
@@ -110,8 +112,8 @@ import cloudpickle
 from .. import jobmanager
 
 
-STACK_NAME = os.getenv('RIOS_BATCH_STACK', default='RIOS')
-REGION = os.getenv('RIOS_BATCH_REGION', default='ap-southeast-2')
+STACK_NAME = os.getenv('RIOS_AWSBATCH_STACK', default='RIOS')
+REGION = os.getenv('RIOS_AWSBATCH_REGION', default='ap-southeast-2')
 
 
 class AWSBatchException(Exception):
@@ -122,7 +124,7 @@ def getStackOutputs():
     """
     Helper function to query the CloudFormation stack for outputs.
     
-    Uses the RIOS_BATCH_STACK and RIOS_BATCH_REGION env vars to 
+    Uses the RIOS_AWSBATCH_STACK and RIOS_AWSBATCH_REGION env vars to 
     determine which stack and region to query.
     """
     client = boto3.client('cloudformation', region_name=REGION)
