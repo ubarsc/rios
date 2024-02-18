999e93d8ffa8b6b7a5a601847c0657133b497bb1
Neil Flood <neilflood@fastmail.fm>
2024-02-18 12:23:47 +1000

    Added rios_computeworker.py

---
+++ rios/cmdline/rios_computeworker.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""
+Main script for a compute worker
+"""
+import argparse
+
+from osgeo import gdal
+
+from rios import applier
+
+
+def getCmdargs():
+    """
+    Get command line arguments
+    """
+    p = argparse.ArgumentParser(description="""
+        Main script run by each RIOS batch system compute worker. A range
+        of command line options are used to control the desired mode of
+        operation.
+    """)
+    p.add_argument("-i", "--idnum", type=int, help="Worker ID number")
+    p.add_argument("--channaddrfile", help="File with data channel address")
+    p.add_argument("--channaddr", help=("Directly specified data channel " +
+        "address, as 'hostname,portnum,authkey'. This is less secure, and " +
+        "should only be used if the preferred option --channaddrfile " +
+        "cannot be used"))
+
+    cmdargs = p.parse_args()
+    return cmdargs
+
+
+def mainCmd():
+    """
+    Main entry point for command script. This is referenced by the install
+    configuration to generate the actual command line main script.
+    """
+    gdal.UseExceptions()
+
+    cmdargs = getCmdargs()
+
+    if cmdargs.channaddrfile is not None:
+        addrStr = open(cmdargs.channaddrfile).readline().strip()
+    else:
+        addrStr = cmdargs.channaddr
+
+    (host, port, authkey) = tuple(addrStr.split(','))
+    port = int(port)
+
+    dataChan = NetworkDataChan(hostname=host, portnum=port, authkey=authkey)
+
+    riosComputeWorker(cmdargs.idnum, dataChan)
+
+
+def riosComputeWorker(workerID, dataChan):
+    """
+    The main routine to run a compute worker. Has options for a number of
+    different modes of operation.
+
+    """
+    userFunction = dataChan.workerCommonData.get('userFunction', None)
+    infiles = dataChan.workerCommonData.get('infiles', None)
+    outfiles = dataChan.workerCommonData.get('outfiles', None)
+    otherArgs = dataChan.workerCommonData.get('otherArgs', None)
+    controls = dataChan.workerCommonData.get('controls', None)
+    allInfo = dataChan.workerCommonData.get('allInfo', None)
+    workinggrid = dataChan.workerCommonData.get('workinggrid', None)
+    inBlockCache = dataChan.workerCommonData.get('inBlockCache', None)
+    outBlockCache = dataChan.workerCommonData.get('outBlockCache', None)
+
+    blockList = dataChan.workerLocalData.get(workerID, None)
+
+    rtn = applier.apply_singleCompute(userFunction, infiles, outfiles,
+        otherArgs, controls, allInfo, workinggrid, blockList, outBlockCache,
+        inBlockCache)
+
+    dataChan.outqueue.put(rtn.timings)
+    dataChan.outqueue.put(otherArgs)
+
+
+if __name__ == "__main__":
+    mainCmd()
