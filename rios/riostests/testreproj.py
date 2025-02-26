3ba6913e58ada66c9712049bc49b355c0019f258
Neil Flood <neilflood@fastmail.fm>
2025-02-27 08:16:43 +1000

    Add a test which detects the negative-null bug in GDAL 3.9.0/3.9.1

--- rios/riostests/testreproj.py
+++ rios/riostests/testreproj.py
@@ -25,12 +25,15 @@ stable. With this in mind, don't be in a hurry to change the detail of this test
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+
 import numpy
 from osgeo import gdal, osr
-from rios import applier
+from rios import applier, fileinfo, pixelgrid
 
 from . import riostestutils
 from ..pixelgrid import PixelGridDefn
+
 
 TESTNAME = "TESTREPROJECTION"
 
@@ -40,7 +43,8 @@ def run():
     Run the test
     """
     riostestutils.reportStart(TESTNAME)
-    
+
+    allOK = True
     ramp1 = 'ramp1.img'
     ramp2 = 'ramp2.img'
     nullval = 0
@@ -50,15 +54,21 @@ def run():
     reprojFile(ramp1, ramp2, resample, nullval)
     
     ok = checkMatch(ramp1, ramp2, resample)
+    allOK = allOK and ok
+
+    # Test reprojection with a negative null value. This had problems in gdal 3.9,
+    # but we now have a work-around.
+    ok = checkNegativeNull()
+    allOK = allOK and ok
     
     # Clean up
     for filename in [ramp1, ramp2]:
         riostestutils.removeRasterFile(filename)
 
-    if ok:
+    if allOK:
         riostestutils.report(TESTNAME, "Passed")
 
-    return ok
+    return allOK
 
 
 def reprojFile(ramp1, ramp2, resampleMethod, nullval):
@@ -146,3 +156,104 @@ def userFunc(info, inputs, outputs, otherargs):
     mismatchCount = numpy.count_nonzero(~match)
     otherargs.mismatchCount += mismatchCount
     otherargs.pixelCount += inputs.img1.size
+
+
+def checkNegativeNull():
+    """
+    Check that reprojection still works when the null value is negative. There
+    was a bug introduced in GDAL 3.9.0, and fixed in 3.9.2, which meant this
+    could fail under some circumstances. We have a workaround, in imagereader.py,
+    and this test is in place to check that workaround.
+    """
+    # Create a file to use as input
+    filename = 'negnull.img'
+    numRows = numCols = 100
+    ds = riostestutils.createTestFile(filename, dtype=gdal.GDT_Int16,
+        numRows=numRows, numCols=numCols, numBands=2)
+    nullval = -10
+    fillval = 50
+    arr = numpy.full((numRows, numCols), fillval, dtype=numpy.int16)
+    arr[:10, :10] = nullval
+
+    # The GDAL bug we are testing only occurs when there are multiple bands
+    for i in [1, 2]:
+        band = ds.GetRasterBand(i)
+        band.WriteArray(arr)
+        band.SetNoDataValue(nullval)
+    del ds
+
+    # Now read it with RIOS, in a different projection.
+    # We resample using cubic convolution, so that if the null value has not
+    # been correctly interpreted, we would get smearing of data into nulls.
+    # If it is correct, the only non-null value will be fillval
+    infiles = applier.FilenameAssociations()
+    outfiles = applier.FilenameAssociations()
+    otherargs = applier.OtherInputs()
+    controls = applier.ApplierControls()
+
+    infiles.img = filename
+    otherargs.nullval = nullval
+    otherargs.fillval = fillval
+    otherargs.ok = True
+    otherargs.msg = None
+    pixgrid = makeNewPixgrid(filename)
+    controls.setReferencePixgrid(pixgrid)
+    controls.setResampleMethod('cubic')
+
+    try:
+        applier.apply(doNeg, infiles, outfiles, otherargs, controls=controls)
+        ok = otherargs.ok
+        msg = otherargs.msg
+    except Exception as e:
+        ok = False
+        msg = ("Exception \"{}\" raised when reprojecting " +
+               "with a negative null value").format(str(e))
+
+    if not ok:
+        riostestutils.report(TESTNAME, msg)
+
+    if os.path.exists(filename):
+        riostestutils.removeRasterFile(filename)
+
+    return ok
+
+
+def doNeg(info, inputs, outputs, otherargs):
+    """
+    Reads input with a reprojection, and check null handling
+    """
+    nullval = otherargs.nullval
+    nonnullMask = (inputs.img[0] != nullval)
+    nonnull = inputs.img[0][nonnullMask]
+    minPixval = nonnull.min()
+    maxPixval = nonnull.max()
+    fillval = otherargs.fillval
+    # The original raster was filled with fillval. If the null was
+    # correctly handled, that is all that should be in the non-null area.
+    # If anything else is present, it has appeared by smearing during resample
+    if minPixval != fillval or maxPixval != fillval:
+        # We have an error in current block. Record it on otherargs,
+        # if not already there from an earlier block
+        if otherargs.ok:
+            otherargs.ok = False
+            otherargs.msg = "Negative null value mis-handled in repprojection"
+
+
+def makeNewPixgrid(filename):
+    """
+    Make a pixgrid for the reprojected version of the input file
+    """
+    outEPSG = 3577
+    info = fileinfo.ImageInfo(filename)
+    corners = info.getCorners(outEPSG=outEPSG)
+    sr = osr.SpatialReference()
+    sr.ImportFromEPSG(outEPSG)
+    wkt = sr.ExportToWkt()
+    (ul_x, ul_y, ur_x, ur_y, lr_x, lr_y, ll_x, ll_y) = corners
+    xMin = min(ul_x, ll_x)
+    xMax = max(ur_x, lr_x)
+    yMin = min(ll_y, lr_y)
+    yMax = max(ul_y, ur_y)
+    pixgrid = pixelgrid.PixelGridDefn(projection=wkt, xMin=xMin, xMax=xMax,
+        yMin=yMin, yMax=yMax, xRes=info.xRes, yRes=info.yRes)
+    return pixgrid
