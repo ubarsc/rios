e7d011dff6b2ef3fd0e2c47cbede27760d299fb0
Neil Flood <neilflood@fastmail.fm>
2025-03-24 14:04:07 +1000

    A timer on starting compute workers (#132)

--- rios/structures.py
+++ rios/structures.py
@@ -836,8 +836,8 @@ class Timers:
             "{:20s}       {:11s}".format("Timer", "Total (sec)"),
             ("-" * 32)
         ]
-        fieldOrder = ['reading', 'userfunction', 'writing', 'pyramids',
-            'basicstats', 'stats+histogram', 'histogram',
+        fieldOrder = ['reading', 'startcomputeworkers', 'userfunction',
+            'writing', 'pyramids', 'basicstats', 'stats+histogram', 'histogram',
             'insert_readbuffer', 'pop_readbuffer', 'insert_computebuffer',
             'pop_computebuffer']
         for name in fieldOrder:
