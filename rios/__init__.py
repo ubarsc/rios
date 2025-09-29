47b757921bda8000a1a299e70481276859603bd0
Neil Flood <neilflood@fastmail.fm>
2025-09-30 09:31:34 +1000

    Ensure GPL license summary is included at top of all main Python files

--- rios/__init__.py
+++ rios/__init__.py
@@ -18,6 +18,22 @@ Rios is built on top of GDAL, and handles, among other things:
 The main starting point is the rios.applier.apply function.
 
 """
+# This file is part of RIOS - Raster I/O Simplification
+# Copyright (C) 2012  Sam Gillingham, Neil Flood
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Used to fill in the rest of the comparison methods
 from functools import total_ordering
 
--- rios/computemanager.py
+++ rios/computemanager.py
@@ -3,6 +3,22 @@ The ComputeWorkerManager abstract base class is for creating and managing
 a set of compute workers. Each different computeWorkerKind is implemented
 as a concrete subclass of this. All these classes are found in this module.
 """
+# This file is part of RIOS - Raster I/O Simplification
+# Copyright (C) 2012  Sam Gillingham, Neil Flood
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 from abc import ABC, abstractmethod
